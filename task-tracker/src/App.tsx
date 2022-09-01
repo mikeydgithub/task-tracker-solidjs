d267929bed76a9db9959b869eeb914f6c96bb4b5
Mike <ampgzo2@gmail.com>
2022-09-01 10:15:59 -0400

    create button and input layout

--- task-tracker/src/App.tsx
+++ task-tracker/src/App.tsx
@@ -1,26 +1,26 @@
 import type { Component } from 'solid-js';
 
-import logo from './logo.svg';
-import styles from './App.module.css';
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+    <div class="container mt-5 text-center">
+      <h1 class="mb-4">Whattodo!</h1>
+
+      <form class="mb-5 row row-cols-2 justify-content-center">
+        <input type="text" class="input-group-text p-1 w-25" placeholder='Add taske here..'/>
+          <button class='btn btn-primary ms-3 w-auto' type='submit' > Add Task </button>
+      </form>
+
+      <div>
+        <h4 class='text-muted mb-4 justify-content-center'>Tasks</h4>
+        <div class="row row-cols-3 mb-3 justify-content-center">
+          <button class='btn btn-danger w-auto'>X</button>
+        
+        <div class='bg-light p2 mx-2'> Push code to GitHub</div>
+          <input type='checkbox' role="button" class='form-check-input h-auto px-3'/>
+        </div>
+      </div>
+  </div>
   );
 };
 
--- task-tracker/src/index.tsx
+++ task-tracker/src/index.tsx
@@ -1,7 +1,6 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
-
-import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 render(() => <App />, document.getElementById('root') as HTMLElement);
